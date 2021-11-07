62bcf99ad880609c843170c4a4ade6dd452ab862
luxeivan <31414039+luxeivan@users.noreply.github.com>
2021-11-07 16:29:21 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -14,6 +14,9 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  percent = Number(percent);
+  contribution = Number(contribution);
+  amount = Number(amount);
   if (typeof (percent) != "number") {
     return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`
   }
