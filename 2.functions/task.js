f2fd34e7a6d895fa0b2db53b309e4f934459ff19
luxeivan <luxeivan@gmail.com>
2021-11-11 21:41:35 +0300

    finishHomeWork

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,45 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
+  let min = arr[0], max = arr[0], sum = 0, avg;
+  for (let i = 0; i < arr.length; i += 1) {
+    arr[i] < min ? min = arr[i] : false;
+    arr[i] > max ? max = arr[i] : false;
+    sum += arr[i];
+  }
+  avg = Number((sum / arr.length).toFixed(2));
   // Ваш код
-
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    sum += arr[i];
+  }
   // Ваш код
-
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i += 1) {
+    func(arrOfArr[i]) > max ? max = func(arrOfArr[i]) : false;
+  }
   // Ваш кода
   // for ...
-  
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
+  let min = arr[0], max = arr[0], difference;
+  for (let i = 0; i < arr.length; i += 1) {
+    arr[i] < min ? min = arr[i] : false;
+    arr[i] > max ? max = arr[i] : false;
+  }
+  difference = max - min;
+  difference < 0 ? difference = difference * -1 : false;
   // Ваш код
+  return difference;
 }
