410b44e2baf1d1e1efe2c862a17d8a5e2f801a9a
luxeivan <luxeivan@gmail.com>
2021-11-21 22:50:19 +0300

    first task

---
+++ 5.classes/task.js
@@ -0,0 +1,58 @@
+class PrintEditionItem {
+    #state;
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.#state = 100;
+        this.type = null;
+    }
+    fix() {
+        if (this.#state > 0 && this.#state < 100) {
+            this.state = this.#state * 1.5;
+        }
+    }
+    set state(state) {
+        if (state < 0) {
+            this.#state = 0;
+        } else if (state > 100) {
+            this.#state = 100;
+        } else {
+            this.#state = state;
+        }
+    }
+    get state() {
+        return this.#state;
+    }
+}
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "magazine";
+    }
+}
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = "book";
+    }
+}
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "novel";
+    }
+}
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "fantastic";
+    }
+}
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "detective";
+    }
+}
