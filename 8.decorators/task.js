12028b94260ea216591cc4f0c837e34fb43c7ee6
luxeivan <luxeivan@gmail.com>
2021-12-05 20:56:49 +0300

    task1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,20 @@
 function cachingDecoratorNew(func) {
   // Ваш код
+  const cache = [];
+  return function wrapper(...args) {
+    let hash = args.join(''),
+      index = cache.findIndex((item) => item.hasOwnProperty(hash));
+    if (index >= 0) {
+      return 'Из кэша: ' + cache[index][hash];
+    } else {
+      let result = func(...args)
+      cache.push({[hash]: result});
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      return 'Вычисляем: ' + result;
+    }
+  }
 }
 
 
@@ -10,3 +25,14 @@ function debounceDecoratorNew(func) {
 function debounceDecorator2(func) {
   // Ваш код
 }
+
+const addThree = (a, b, c) => a + b + c;
+const upgradedAddThree = cachingDecoratorNew(addThree);
+upgradedAddThree(1, 2, 3); // вычисляем: 6
+upgradedAddThree(1, 2, 3); // из кэша: 6
+upgradedAddThree(2, 2, 3); // вычисляем: 7
+upgradedAddThree(3, 2, 3); // вычисляем: 8
+upgradedAddThree(4, 2, 3); // вычисляем: 9
+upgradedAddThree(5, 2, 3); // вычисляем: 10
+upgradedAddThree(6, 2, 3); // вычисляем: 11   (при этом кэш для 1, 2, 3 уничтожается)
+upgradedAddThree(1, 2, 3); // вычисляем: 6  (снова вычисляем, кэша нет)
